b3a5edc7858d594c471ef0df31d176ad9646305a
Reza Sajadiany <rezasjd@berkeley.edu>
2024-02-05 06:37:03 +0000

    lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,21 +1,28 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+lsp.extend_lspconfig()
 
 lsp.setup()
 
-
 local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select), 
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select), 
-	['<C-o>'] = cmp.mapping.confirm({ select = true }), 
-	['<C-space>'] = cmp.mapping.complete(),
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select), 
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select), 
+    ['<C-o>'] = cmp.mapping.confirm({ select = true }), 
+    ['<C-y>'] = cmp.mapping.complete(),
+  })
 })
 
-lsp.setup_nvim_cmp( {
-	mapping = cmp_mappings
-})
+lsp.setup_servers({'rust_analyzer', 'clangd'})
 
 
 lsp.setup()
