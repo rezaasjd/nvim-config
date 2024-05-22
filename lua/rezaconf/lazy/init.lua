1976c3b4c03943ef5352f68b3ee66c0e74f74c65
Reza Sajadiany <rezasjd@berkeley.edu>
2024-05-22 11:51:57 -0700

    colorscheme change

--- lua/rezaconf/lazy/init.lua
+++ lua/rezaconf/lazy/init.lua
@@ -53,7 +53,7 @@ return {
     'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
-      vim.cmd('colorscheme rose-pine')
+      vim.cmd('colorscheme tokyonight-moon')
     end
   },
 
