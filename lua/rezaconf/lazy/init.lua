546b905b7b8bdc3db01fe26328c9c6cc82a840bd
Reza Sajadiany <rezasjd@berkeley.edu>
2024-10-09 14:28:41 -0700

    fugitive in a new tab is better.

--- lua/rezaconf/lazy/init.lua
+++ lua/rezaconf/lazy/init.lua
@@ -14,7 +14,7 @@ return {
   {
     'tpope/vim-fugitive',
     config = function()
-      vim.keymap.set('n', '<leader>g', vim.cmd.Git);
+      vim.keymap.set('n', '<leader>g', ":tab G<CR>");
     end
   },
 
