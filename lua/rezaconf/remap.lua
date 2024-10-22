e8411f3788111c9e3901d55307bf214620a66721
Reza Sajadiany <rezasjd@berkeley.edu>
2024-10-22 11:07:32 -0700

    delete file function within a buffer

--- lua/rezaconf/remap.lua
+++ lua/rezaconf/remap.lua
@@ -65,3 +65,13 @@ end)
 vim.keymap.set('n', '<leader>trn', function()
   vim.opt.relativenumber = true
 end)
+
+-- delete file
+local function confirm_and_delete_buffer()
+  local confirm = vim.fn.confirm("Delete buffer and file?", "&Yes\n&No", 2)
+  if confirm == 1 then
+    os.remove(vim.fn.expand "%")
+    vim.api.nvim_buf_delete(0, { force = true })
+  end
+end
+vim.keymap.set('n', '<leader>d', confirm_and_delete_buffer)
