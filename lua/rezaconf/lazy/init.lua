7c83f0a03a2e1f55a3c8ada7426bf0a971920f1e
rsajadia <reza.sajadiany@amd.com>
2025-03-09 21:31:46 -0700

    Indentation guide

--- lua/rezaconf/lazy/init.lua
+++ lua/rezaconf/lazy/init.lua
@@ -58,4 +58,14 @@ return {
     end
   },
 
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+    config = function()
+      require("ibl").setup()
+    end
+  },
 }
