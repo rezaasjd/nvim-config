e24db52a2e5e637362775d8d25238cf66a843d6f
Reza Sajadiany <rezasaj@meta.com>
2026-06-22 14:05:01 -0700

    file grab

--- lua/rezaconf/remap.lua
+++ lua/rezaconf/remap.lua
@@ -45,6 +45,11 @@ vim.keymap.set('t', 'jk', [[<C-\><C-n>]], { noremap = true })
 -- autocomplete from buffer
 --vim.keymap.set('i', '<C-f>', '<C-x><C-n>')
 
+vim.keymap.set('n', '<leader>cp', function()
+  local path = vim.fn.expand('%:p')
+  vim.fn.setreg('+', path)
+  vim.notify('Copied: ' .. path)
+end)
 
 -- copycat
 vim.keymap.set('n', '<leader>y', '"*y')
